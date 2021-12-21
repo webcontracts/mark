8df5f6b125763ef1c3bb64464db7d4ee6a1d0261
Melvin Carvalho <https://melvincarvalho.com/#me>
2021-12-21 19:52:51 +0100

    finish contract and bump package

--- webcontracts/mark.js
+++ webcontracts/mark.js
@@ -6,6 +6,42 @@ import minimist from 'minimist'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+// CONTRACT
+function mark(source, amount, currency, destination, timesamp, description, context, indir, infile) {
+  var credit = { "@type": "Credit" }
+  if (source) credit.source = source
+  if (amount) credit.amount = amount
+  if (currency) credit.currency = currency
+  if (destination) credit.destination = destination
+  if (timestamp) credit.timestamp = timestamp
+  console.log('credit', credit)
+  if (description) credit.description = description
+  if (context) credit.context = context
+
+  // MAIN
+  var credits = []
+  try {
+    var d = fs.readFileSync(infile)
+    var credits = JSON.parse(d)
+  } catch (e) {
+    if (!fs.existsSync(indir)) {
+      console.log('making dir', indir)
+      fs.mkdirSync(indir)
+    }
+    console.log('creating', infile)
+    var d = JSON.stringify([], null, 1)
+    fs.writeFileSync(infile, d)
+  }
+
+  if (data.amount) {
+    credits.push(credit)
+  }
+
+  // WRITE
+  fs.writeFileSync(infile, JSON.stringify(credits, null, 1))
+}
+
+
 // INIT
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -33,41 +69,8 @@ data.context = argv.context || data.context
 data.indir = argv.indir || data.indir
 data.infile = argv.infile || data.infile
 
-function mark(source, amount, currency, destination, timesamp, description, context, indir, infile) {
-  var credit = { "@type": "Credit" }
-  if (data.source) credit.source = data.source
-  if (data.amount) credit.amount = data.amount
-  if (data.currency) credit.currency = data.currency
-  if (data.destination) credit.destination = data.destination
-  if (data.timestamp) credit.timestamp = data.timestamp
-  console.log('credit', credit)
-  if (data.description) credit.description = data.description
-  if (data.context) credit.context = data.context
-
-  // MAIN
-  var credits = []
-  try {
-    var d = fs.readFileSync(infile)
-    var credits = JSON.parse(d)
-  } catch (e) {
-    if (!fs.existsSync(indir)) {
-      console.log('making dir', indir)
-      fs.mkdirSync(indir)
-    }
-    console.log('creating', infile)
-    var d = JSON.stringify([], null, 1)
-    fs.writeFileSync(infile, d)
-  }
-
-  if (data.amount) {
-    credits.push(credit)
-  }
-
-  // WRITE
-  fs.writeFileSync(infile, JSON.stringify(credits, null, 1))
-}
-
-
+// MAIN
 mark(data.source, data.amount, data.currency, data.destination, data.timesamp, data.description, data.context, indir, infile)
 
+// EXPORT
 export default mark
